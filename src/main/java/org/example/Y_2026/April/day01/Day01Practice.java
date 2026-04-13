a329e968a355a52365c3b99020e1efaa46301437
Wonjin <Wonjin@DESKTOP-GKOEREA>
2026-04-13 09:06:26 +0900

    first

---
+++ src/main/java/org/example/Y_2026/April/day01/Day01Practice.java
@@ -0,0 +1,15 @@
+package org.example.Y_2026.April.day01;
+
+/**
+ * 프로그래머스 Level 1 | #42748 - K번째수
+ *
+ *
+ * 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하세요.
+ */
+public class Day01Practice {
+
+    public static void main(String[] args) {
+
+    }
+
+}
