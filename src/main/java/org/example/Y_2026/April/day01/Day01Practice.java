00235d878f10bc4a2010a69484b7eafa2bfefd3e
Wonjin <Wonjin@DESKTOP-GKOEREA>
2026-04-13 10:46:16 +0900

    260413 정원진
    알고리즘 - K번째수
    
    1. array라는 배열 내 i~j의 숫자 중 k번째 숫자를 떼내오려고한다.
    2. commands라는 숫자 픽을 위한 i,j,k를 상수 배열 변수가 따라온다.
    3. commands의 갯수만큼 루프를 반복
    4. 반복 루프에서 commands 배열마다 i,j,k를 꺼내서 원시타입화
    5. Arrays.copyOfRange로 해당 객체를 직접 자르지 않고 복사 처리
    6. 잘라낸 범위 숫자를 answer에 담아준다.

--- src/main/java/org/example/Y_2026/April/day01/Day01Practice.java
+++ src/main/java/org/example/Y_2026/April/day01/Day01Practice.java
@@ -1,14 +1,38 @@
 package org.example.Y_2026.April.day01;
 
+import java.util.Arrays;
+
 /**
  * 프로그래머스 Level 1 | #42748 - K번째수
  *
  *
- * 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하세요.
+ * 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때
+ * k번째에 있는 수를 구하세요.
  */
 public class Day01Practice {
 
     public static void main(String[] args) {
+
+        // 테스트 기반
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2,5,3}, {4,4,1}, {1,7,3}};
+
+        int[] answer = new int[commands.length];
+
+        for (int idx = 0; idx < commands.length; idx++) {
+            int[] cmd = commands[idx];
+            int i = cmd[0];
+            int j = cmd[1];
+            int k = cmd[2];
+
+
+            int[] sliced = Arrays.copyOfRange(array, i-1, j);
+            Arrays.sort(sliced);
+            answer[idx] = sliced[k-1];
+
+        }
+
+        System.out.println(Arrays.toString(answer));
 
     }
 
