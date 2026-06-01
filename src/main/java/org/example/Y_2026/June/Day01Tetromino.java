42428def3e85eb28253483e6dd960ccff2ad2782
Wonjin <Wonjin@DESKTOP-GKOEREA>
2026-06-01 09:03:06 +0900

    260601 정원진
    Day01 테트라미노
    
    1. 문제 준비

---
+++ src/main/java/org/example/Y_2026/June/Day01Tetromino.java
@@ -0,0 +1,21 @@
+package org.example.Y_2026.June;
+
+import java.util.Scanner;
+
+/**
+ * Day01 테트로미노
+ *
+ * 폴리오미노란 크기가 1×1인 정사각형을 여러 개 이어 붙인 도형이다.
+ * 테트로미노는 정사각형 4개를 이어 붙인 도형으로, 회전·대칭 포함 5가지 종류가 있다.
+ * N×M 크기의 종이에 각 칸마다 숫자가 적혀 있을 때, 테트로미노 하나를 종이 위에 올려놓고 4개 칸의 숫자 합이 최대가 되도록 하라.
+ *
+ */
+public class Day01Tetromino {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
