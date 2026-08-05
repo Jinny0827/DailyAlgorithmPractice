902fb6fbd8cec5749f4a22474c94e1d65b2bb799
Wonjin <Wonjin@DESKTOP-GKOEREA>
2026-08-05 11:39:15 +0900

    260805 정원진
    Day64 최솟값 구하기
    
    1. N개의 갯수 값, L개의 정수를 받아 N개만큼 반복하며 A배열에 넣는다.
    2. 단조덱 계산기법으로(slidingWindow 함수) 인덱스를 보관하는 deck 배열 / 실제 L개 값들 중 최소값을 저장할 D 배열 선언
    -> head, tail을 0과 -1로 선언하여 A의 배열에서 조건에 따라 포인터를 이동시키며 최소값 확인
    3. 갯수 값 N개에 따라 아래 조건들로 head와 tail을 이동시킨다.
    -> 맨뒤인덱스가 맨앞인덱스보다 크거나 같고 맨뒤인덱스의 배열값이 현재 인덱스 배열값보다 크거나 같으면 맨뒤 인덱스를 줄여서 바라보는 값의 범위를 축소시킨다.
    -> 맨뒤인덱스값을 추가시키며 i를 넣는다.
    -> 맨앞인덱스값이 현재 인덱스 - L(윈도우 크기)보다 작거나 같으면 인덱스를 한칸 앞으로 이동시킨다. (L 범위 밖의 경우)
    -> 맨앞인덱스는 최소값이기때문에 실제 저장배열 D에 값을 추가시킨다.
    4. slidingWindow 함수에서 N번의 반복하며 저장된 최소값 배열을 StringBuilder를 통해 한칸씩 띄워가며 출력해준다.

---
+++ src/main/java/org/example/Y_2026/August/Day64SlidingWindow.java
@@ -0,0 +1,83 @@
+package org.example.Y_2026.August;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Day64 최솟값 구하기
+ *
+ * N개의 수 A1, A2, ..., AN과 정수 L이 주어진다.
+ * Di를 Ai-L+1 ~ Ai 중 최솟값이라 정의한다. 단, i-L+1 < 1이면 A1부터 Ai까지의 최솟값으로 한다. D1, D2, ..., DN을 구하라.
+ *
+ * 입력 >>
+ * 첫째 줄: N, L (1 ≤ L ≤ N ≤ 5,000,000)
+ * 둘째 줄: Ai (-10^9 ≤ Ai ≤ 10^9), N개
+ *
+ * 출력 >>
+ * D1부터 DN까지 공백으로 구분하여 출력
+ * 
+ */
+public class Day64SlidingWindow {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] result = slidingWindow(A, N, L);
+        for(int i = 0; i < N; i++) {
+            sb.append(result[i]).append(" ");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    private static int[] slidingWindow(int[] arr, int N, int L) {
+        // 단조덱 사용
+        // 덱(인덱스 저장용)과 결과 배열 D를 준비한다.
+        int[] deck  = new int[N];
+        // 실제 원소가 있는 범위 (head,tail)
+        int head = 0;
+        int tail = -1;
+        int[] D = new int[N];
+
+        // i를 0부터 N-1까지 순회하며 아래를 반복한다.
+        for(int i = 0; i < N; i++) {
+            
+            // 덱이 비어있지 않고, 덱 맨 뒤 인덱스의 값이 A[i]보다 크거나 같으면 → 맨 뒤를 계속 제거한다.
+            while(head <= tail && arr[deck[tail]] >= arr[i]) {
+                // 맨뒤인덱스가 앞인덱스보다 크고 맨뒤인덱스값이이 현재인덱스값보다 크면 맨뒤인덱스의 범위를 줄여준다. -> 맨뒤인덱스를 작은값으로 이동
+                tail--;
+            }
+
+            // 현재 인덱스 i를 덱 맨 뒤에 추가한다.
+            // 현재 인덱스가 L의 범위를 넘지 않을 때 맨뒤덱 인덱스 위치를 한칸뒤로 이동 -> 맨뒤인덱스 큰값으로 이동
+            deck[++tail] = i;
+
+            // 덱 맨 앞 인덱스가 윈도우 범위(i-L)보다 작거나 같으면(범위 밖이면) → 맨 앞을 제거한다.
+            // 혹은 L의 범위에 도달 못했다면 범위 도달까지 head값 추가
+            if(deck[head] <= i - L) {
+                // 맨앞인덱스가 현인덱스 - 인덱스범위값보다 작으면 맨앞 인덱스를 한칸 앞으로 이동 -> 맨앞인덱스 큰값으로 이동
+                head++;
+            }
+
+            // D[i] = A[맨앞인덱스] 로 저장한다.
+            D[i] = arr[deck[head]];
+        }
+
+        // 순회가 끝나면 D 배열이 정답이다.
+        return D;
+    }
+
+}
